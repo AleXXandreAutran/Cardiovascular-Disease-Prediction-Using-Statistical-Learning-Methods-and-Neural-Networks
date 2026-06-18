7d928565c4be34df699fc0657a279def3c0d873a
Alexandre Autran <alexandre.autran13@gmail.com>
2026-06-18 18:19:31 +0200

    Update cardio_14_permutation_importance.py

--- cardiovascular_disease_prediction_python_files/cardio_14_permutation_importance.py
+++ cardiovascular_disease_prediction_python_files/cardio_14_permutation_importance.py
@@ -1,5 +1,3 @@
-# Generated from Code_cardiovascular_disease_prediction.ipynb
-# One file corresponds to one non-empty code cell from the notebook.
 # Run the files in numeric order, or use run_all.py.
 
 rf_pipe = models["Random forest"].best_estimator_
