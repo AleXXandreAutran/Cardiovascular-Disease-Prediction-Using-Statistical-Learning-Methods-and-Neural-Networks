43c42bd058708a7437b402b9534450666d16147f
Alexandre Autran <alexandre.autran13@gmail.com>
2026-06-18 18:20:16 +0200

    Update cardio_10_roc_curves.py

--- cardiovascular_disease_prediction_python_files/cardio_10_roc_curves.py
+++ cardiovascular_disease_prediction_python_files/cardio_10_roc_curves.py
@@ -1,5 +1,3 @@
-# Generated from Code_cardiovascular_disease_prediction.ipynb
-# One file corresponds to one non-empty code cell from the notebook.
 # Run the files in numeric order, or use run_all.py.
 
 plt.figure(figsize=(7.2, 5.5))
